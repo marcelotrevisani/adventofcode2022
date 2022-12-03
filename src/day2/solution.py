a7101e55f51a21048d0d0d9aa4af2a5b2282827c
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2022-12-03 22:17:22 +0000

    Solution day 2 - Part 2 - it can be refactored

--- src/day2/solution.py
+++ src/day2/solution.py
@@ -2,17 +2,20 @@ import re
 from pathlib import Path
 
 
-def solve(txt_input):
-    total_points = 0
+def solve_part_1(txt_input):
     re_turns = re.compile(r"(\w)\s+(\w)")
+    turns = [re_turns.match(turn).groups() for turn in txt_input]
+    return calculate_points(turns)
+
+
+def calculate_points(all_turns):
     fixed_points = {
         "X": 1,
         "Y": 2,
         "Z": 3,
     }
-
-    for turn in txt_input:
-        turn = re_turns.match(turn).groups()
+    total_points = 0
+    for turn in all_turns:
         match turn:
             case ("A", "Y") | ("B", "Z") | ("C", "X"):
                 total_points += 6
@@ -22,7 +25,27 @@ def solve(txt_input):
     return total_points
 
 
+def solve_part_2(txt_input):
+    re_turns = re.compile(r"(\w)\s+(\w)")
+    all_turns = []
+    seq_pos = ("X", "Y", "Z")
+    seq_val = {j: i for i, j in enumerate(seq_pos)}
+    mapping = {"A": "X", "B": "Y", "C": "Z"}
+    for turn in txt_input:
+        turn = re_turns.match(turn).groups()
+        result = turn[1]
+        match result:
+            case "X":
+                all_turns.append((turn[0], seq_pos[seq_val[mapping[turn[0]]] - 1]))
+            case "Y":
+                all_turns.append((turn[0], mapping[turn[0]]))
+            case "Z":
+                all_turns.append((turn[0], seq_pos[(seq_val[mapping[turn[0]]] + 1) % len(seq_pos)]))
+    return calculate_points(all_turns)
+
+
 if __name__ == "__main__":
     with open(Path(__file__).parent / "input.txt", "r") as f:
         txt_input = f.readlines()
-    print(f"Day 2, part 1: {solve(txt_input)}")
+    print(f"Day 2, part 1: {solve_part_1(txt_input)}")
+    print(f"Day 2, part 2: {solve_part_2(txt_input)}")
--- src/day2/test_solution.py
+++ src/day2/test_solution.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from day2.solution import solve
+from day2.solution import solve_part_1, solve_part_2
 
 
 def test_solve():
     with open(Path(__file__).parent / "test_input.txt", "r") as f:
         txt_input = f.readlines()
-    assert solve(txt_input) == 15
+    assert solve_part_1(txt_input) == 15
+    assert solve_part_2(txt_input) == 12
