08e5283e3dacc7113f2120546bac957b5ed9ec41
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2022-12-03 13:43:37 +0000

    Solution day 1 - part 2

--- src/day1/solution.py
+++ src/day1/solution.py
@@ -1,3 +1,4 @@
+import heapq
 from pathlib import Path
 
 
@@ -10,9 +11,25 @@ def solve(txt_input):
         except ValueError:
             max_val = max(max_val, single_accumulator)
             single_accumulator = 0
-    return max_val
+    return max(max_val, single_accumulator)
+
+
+def solve_part2(txt_input):
+    accumulator = 0
+    all_values = []
+    for line in txt_input:
+        try:
+            accumulator += int(line)
+        except ValueError:
+            all_values.append(accumulator)
+            accumulator = 0
+    if accumulator > 0:
+        all_values.append(accumulator)
+    return sum(heapq.nlargest(3, all_values))
 
 
 if __name__ == "__main__":
     with open(Path(__file__).parent / "input.txt", "r") as f:
-        print(solve(f.readlines()))
+        all_input = f.readlines()
+    print(f"Part 1: {solve(all_input)}")
+    print(f"Part 2: {solve_part2(all_input)}")
--- src/day1/test_solution.py
+++ src/day1/test_solution.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 
-from day1.solution import solve
+from day1.solution import solve, solve_part2
 
 
-def test_solution():
+def test_solution_part1():
     test_input_file = Path(__file__).parent / "test_input.txt"
     with open(test_input_file, "r") as f:
         test_input_file = f.readlines()
     assert solve(test_input_file) == 24000
+
+def test_solution_part2():
+    test_input_file = Path(__file__).parent / "test_input.txt"
+    with open(test_input_file, "r") as f:
+        test_input_file = f.readlines()
+    assert solve_part2(test_input_file) == 45000
