a2b191feac35fac7b8781d05b5e5816674e99c2d
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2022-12-04 22:42:17 +0000

    Solution day 4 - Part 2

--- src/day4/solution.py
+++ src/day4/solution.py
@@ -14,7 +14,20 @@ def solve_part_1(txt_input):
     return total
 
 
+def solve_part_2(txt_input):
+    total = 0
+    re_elf = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+    for line in txt_input:
+        start_1, end_1, start_2, end_2 = [int(i) for i in re_elf.match(line).groups()]
+        if (start_1 > end_2) or (start_2 > end_1):
+            continue
+        else:
+            total += 1
+    return total
+
+
 if __name__ == "__main__":
     with open(Path(__file__).parent / "input.txt", "r") as f:
         txt_input = f.readlines()
     print(f"Day 4 - Part 1: {solve_part_1(txt_input)}")
+    print(f"Day 4 - Part 2: {solve_part_2(txt_input)}")
--- src/day4/test_solution.py
+++ src/day4/test_solution.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from day4.solution import solve_part_1
+from day4.solution import solve_part_1, solve_part_2
 
 
 def test_solution():
     with open(Path(__file__).parent / "test_input.txt", "r") as f:
         txt_input = f.readlines()
     assert solve_part_1(txt_input) == 2
+    assert solve_part_2(txt_input) == 4
